351fed3b17a2187ea0b7d81d7788dbb66e55a239
qiuhaoding2022 <qiuhaoding2022@u.northwestern.edu>
2021-04-06 16:35:11 -0500

    update plot

--- HW2/MAF/Q5_MA.py
+++ HW2/MAF/Q5_MA.py
@@ -4,7 +4,7 @@ import csv
 
 t = [] # column 0
 data = [] # column 1
-with open('sigC.csv') as f:
+with open('sigD.csv') as f:
     # open the csv file
     reader = csv.reader(f)
     for row in reader:
@@ -12,7 +12,7 @@ with open('sigC.csv') as f:
         t.append(float(row[0])) # leftmost column
         data.append(float(row[1])) # second column
 
-X=1 # number of data points averaged
+X=20 # number of data points averaged
 newdata=[0]*(X-1)
 i=0
 while i+X<=len(t):
@@ -20,6 +20,11 @@ while i+X<=len(t):
     newdata.append(sum(avglist)/X)
     i=i+1
 
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(t,data,'k')
+ax1.plot(t,newdata,'r')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Amplitude')
 
 
 Fs = len(t)/(t[-1]-t[0]) #sampling rate
@@ -34,7 +39,7 @@ frq = frq[range(int(n/2))] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(int(n/2))]
 
-plt.loglog(frq,abs(Y),'k') # plotting the fft
+ax2.loglog(frq,abs(Y),'k') # plotting the fft
 
 Fs = len(t)/(t[-1]-t[0]) #sampling rate
 Ts = 1.0/Fs # sampling interval
@@ -48,13 +53,13 @@ frq = frq[range(int(n/2))] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(int(n/2))]
 
-plt.loglog(frq,abs(Y),'r') # plotting the fft
+ax2.loglog(frq,abs(Y),'r') # plotting the fft
 
 
-plt.xlabel('Freq (Hz)')
-plt.ylabel('|Y(freq)|')
+ax2.set_xlabel('Freq (Hz)')
+ax2.set_ylabel('|Y(freq)|')
 title="number of data points averaged = %d " % X
-plt.title(title)
+ax1.set_title(title)
 
 plt.show()
 
