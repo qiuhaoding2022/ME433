91dbc57abc798bb14218d1d02edd78bc1667362f
qiuhaoding2022 <qiuhaoding2022@u.northwestern.edu>
2021-04-06 16:46:22 -0500

    update plot
    FIR

--- HW2/FIR/FIR.py
+++ HW2/FIR/FIR.py
@@ -4,7 +4,7 @@ import csv
 
 t = [] # column 0
 data = [] # column 1
-with open('sigC.csv') as f:
+with open('sigD.csv') as f:
     # open the csv file
     reader = csv.reader(f)
     for row in reader:
@@ -12,10 +12,17 @@ with open('sigC.csv') as f:
         t.append(float(row[0])) # leftmost column
         data.append(float(row[1])) # second column
 h = [
-    0.040387893253289342,
-    0.919224213493421316,
-    0.040387893253289342,
-] #Gaussian weight with samples # = X
+    0.009785867631224162,
+    0.033632981159779557,
+    0.084896519194903930,
+    0.157388464297928743,
+    0.214296167716163610,
+    0.214296167716163610,
+    0.157388464297928743,
+    0.084896519194903930,
+    0.033632981159779557,
+    0.009785867631224162,
+]#Gaussian weight with samples # = X
 
 X=len(h) # number of data points sampled
 newdata=[0]*(X-1)
@@ -28,8 +35,14 @@ while i+X<=len(t):
     i=i+1
 
 
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(t,data,'k')
+ax1.plot(t,newdata,'r')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Amplitude')
+
+
 Fs = len(t)/(t[-1]-t[0]) #sampling rate
-print(Fs)
 Ts = 1.0/Fs # sampling interval
 ts = np.arange(0,t[-1],Ts) # time vector
 y = data # the data to make the fft from
@@ -41,7 +54,7 @@ frq = frq[range(int(n/2))] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(int(n/2))]
 
-plt.loglog(frq,abs(Y),'k') # plotting the fft
+ax2.loglog(frq,abs(Y),'k') # plotting the fft
 
 Fs = len(t)/(t[-1]-t[0]) #sampling rate
 Ts = 1.0/Fs # sampling interval
@@ -55,13 +68,13 @@ frq = frq[range(int(n/2))] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(int(n/2))]
 
-plt.loglog(frq,abs(Y),'r') # plotting the fft
+ax2.loglog(frq,abs(Y),'r') # plotting the fft
 
 
-plt.xlabel('Freq (Hz)')
-plt.ylabel('|Y(freq)|')
+ax2.set_xlabel('Freq (Hz)')
+ax2.set_ylabel('|Y(freq)|')
 title="FIR sample number = %d , Gaussian weighting " % X
-plt.title(title)
+ax1.set_title(title)
 
 plt.show()
 
