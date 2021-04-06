0becafa7504eae4bea444fb73817c005215b08ee
qiuhaoding2022 <qiuhaoding2022@u.northwestern.edu>
2021-04-06 16:40:21 -0500

    plot update

--- HW2/IIR/IIR.py
+++ HW2/IIR/IIR.py
@@ -4,7 +4,7 @@ import csv
 
 t = [] # column 0
 data = [] # column 1
-with open('sigC.csv') as f:
+with open('sigD.csv') as f:
     # open the csv file
     reader = csv.reader(f)
     for row in reader:
@@ -13,13 +13,20 @@ with open('sigC.csv') as f:
         data.append(float(row[1])) # second column
 
 i=1
-A=0.1
+A=0.8
 newdata=[]
 newdata.append(data[0])
 while i<len(t):
     newdata.append(A*newdata[i-1]+(1-A)*data[i])
     i=i+1
 
+
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(t,data,'k')
+ax1.plot(t,newdata,'r')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Amplitude')
 
 
 Fs = len(t)/(t[-1]-t[0]) #sampling rate
@@ -34,7 +41,7 @@ frq = frq[range(int(n/2))] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(int(n/2))]
 
-plt.loglog(frq,abs(Y),'k') # plotting the fft
+ax2.loglog(frq,abs(Y),'k') # plotting the fft
 
 Fs = len(t)/(t[-1]-t[0]) #sampling rate
 Ts = 1.0/Fs # sampling interval
@@ -48,13 +55,12 @@ frq = frq[range(int(n/2))] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(int(n/2))]
 
-plt.loglog(frq,abs(Y),'r') # plotting the fft
+ax2.loglog(frq,abs(Y),'r') # plotting the fft
 
-
-plt.xlabel('Freq (Hz)')
-plt.ylabel('|Y(freq)|')
+ax2.set_xlabel('Freq (Hz)')
+ax2.set_ylabel('|Y(freq)|')
 title="Weight A = %.2f  B = %.2f" % (A, (1-A))
-plt.title(title)
+ax1.set_title(title)
 
 plt.show()
 
