7e0a3fe65303fef7bf015acb230cc8acd4eae8d1
qiuhaoding2022 <qiuhaoding2022@u.northwestern.edu>
2021-05-01 16:24:18 -0500

    update LATA and TRIS
    put TRIS and LAT setup outside the while loop

--- HW4/test/HW4_test.X/main.c
+++ HW4/test/HW4_test.X/main.c
@@ -49,15 +49,16 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
-
+    TRISAbits.TRISA4 = 0;
+    LATAbits.LATA4 = 0;
+    TRISBbits.TRISB4 = 1;
     __builtin_enable_interrupts();
 
     while (1) {
         // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
         // remember the core timer runs at half the sysclk
-        TRISAbits.TRISA4 = 0;
-        LATAbits.LATA4 = 0;
-        TRISBbits.TRISB4 = 1;
+
+        
         if (PORTBbits.RB4 == 0)
         {
             LATAbits.LATA4 = 1;
