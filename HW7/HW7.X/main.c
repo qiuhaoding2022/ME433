f5a78b9f0dba1f9a26f3a3388dadfe8589b3e841
qiuhaoding2022 <qiuhaoding2022@u.northwestern.edu>
2021-05-17 20:05:13 -0500

    HW7 - SPI DAC

---
+++ HW7/HW7.X/main.c
@@ -0,0 +1,89 @@
+// initialize SPI1
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include<stdio.h>
+#include "spi.h"
+#include <math.h>
+#define pi 3.1415926
+// DEVCFG0
+#pragma config DEBUG = OFF // disable debugging
+#pragma config JTAGEN = OFF // disable jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // disable flash write protect
+#pragma config BWP = OFF // disable boot write protect
+#pragma config CP = OFF // disable code protect
+
+// DEVCFG1
+#pragma config FNOSC = FRCPLL // use internal oscillator with pll
+#pragma config FSOSCEN = OFF // disable secondary oscillator
+#pragma config IESO = OFF // disable switching clocks
+#pragma config POSCMOD = OFF // internal RC
+#pragma config OSCIOFNC = OFF // disable clock output
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576 // use largest wdt
+#pragma config WINDIS = OFF // use non-window mode wdt
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+// DEVCFG3
+#pragma config USERID = 36 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+void send_DAC(char AB, unsigned short val);
+
+
+int main() {
+
+    __builtin_disable_interrupts(); // disable interrupts while initializing things
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0;
+    LATAbits.LATA4 = 0;
+    TRISBbits.TRISB4 = 1;
+
+    initSPI();
+    
+    __builtin_enable_interrupts();
+    
+    unsigned char i=0;
+    float vA,vB,t;
+    while(1) {
+        LATAbits.LATA0=1;
+        vA=512+512*sin(2*pi*t);
+        t=t+0.001;
+        send_DAC(1,(unsigned short)vA); //0 A, 1 B, max 2^10
+        _CP0_SET_COUNT(0);
+        while (_CP0_GET_COUNT() <48000000/2*0.001) {}
+    }
+}
+
+void send_DAC(char AB, unsigned short val){
+    unsigned short p = AB << 15;
+    p=p|(0b111<<12);
+    p=p|(val<<2);
+    //p=0b1111111111111100;
+    LATAbits.LATA0=0; //bring CS low
+    spi_io(p>>8);
+    spi_io(p);        ;
+    LATAbits.LATA0=1;
+}
+
+
