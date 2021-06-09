80f65fdc64054e292812f6ab366aa87f302d81c4
qiuhaoding2022 <qiuhaoding2022@u.northwestern.edu>
2021-06-09 00:50:34 -0500

    HW10 _ rainbow led
    WS2812B

---
+++ HW10/LED.X/build/default/production/main.o.d
@@ -0,0 +1,9 @@
+build/default/production/main.o: main.c ws2812b.h ST7789.h font.h spi.h
+
+ws2812b.h:
+
+ST7789.h:
+
+font.h:
+
+spi.h:
---
+++ HW10/LED.X/build/default/production/ws2812b.o.d
@@ -0,0 +1,10 @@
+build/default/production/ws2812b.o: ws2812b.c ws2812b.h ST7789.h spi.h \
+ font.h
+
+ws2812b.h:
+
+ST7789.h:
+
+spi.h:
+
+font.h:
